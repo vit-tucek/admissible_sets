387604bb93d57962773bc1f6d487e3608f7b004e
Vít Tuček <username without diacritics>
2013-06-06 01:49:44 +0200

    added greedy_greedy algorhitm of Andrew Sutherland

--- admissible_sets.py
+++ admissible_sets.py
@@ -1,39 +1,105 @@
 #import pdb
 import numpy as np
-from prime_utils import *
+from prime_utils import * # this module contains prime-related stuff such as pi() as well as checkign for admissibility
+from math import pow, log, sqrt
 
-N = 100000
+#N = 10000
+k = 34429
+N = 500000
 # we always assume that elements of H are in increasing order
-H = np.arange(-N/2,N/2) 
-# H = np.arange(N) # is this worse than symmetric interval around 0?
+H = np.arange(-N/2,N/2)
+#H = np.arange(N) # is this worse than symmetric interval around 0?
 
 def process(H):
-    primes = get_primes()
-    maxH = max(abs(H))
-    while primes[-1] < maxH:
-        add_prime(primes)
+    '''
+    In this function we populate the primes and pick a sieve.
+    '''
+    primes = get_primes(max(abs(H)))
 #    sieve = [(p,1) for p in primes[:len(primes)/5]]
-    sieve = eratosthenes(primes,math.log(N)*math.sqrt(N))
+#    sieve = eratosthenes(primes,N/(math.log(N)*4))
+#    y = 10
+#    x = len(H)
+#    z = x/(0.2*log(x)*pow(log(log(x)),pi(primes,y)))
+#    sieve = schinzel(primes,y,z)
+    sieve = schinzel(primes,20,2000)
     H = sift(H,sieve)
+#    H = greedy(H,primes[pi(primes,z):pi(primes,z)+100])
     return H, primes
-    
+
 def sift(H,sieve):
+    '''
+    This accomplishes the actual sieving.
+    '''
     for p,s in sieve:
         cond = H % p != s
-        H = H.compress(cond)
+        H = H[cond]
 #        if max(abs(H)) < p: # this actually hurts performance
 #            break
     return H
-    
 
 def eratosthenes(primes,cutoff):
+    '''
+    Eratosthenes sieve as described in [GR1998]
+    '''
     cutoff = pi(primes,cutoff) # returns the index of the first prime > N/2
     return [(p,0) for p in primes[:cutoff]]
-    
+
 def schinzel(primes,y,z):
+    '''
+    Schinzel sieve as described in [GR1998]
+    '''
     y = pi(primes,y)
+#    print primes[:y]
     z = pi(primes,z+1)
-    return [(p,1) for p in primes[:y]] + [(p,0) for p in primes[y:z+1]]
-    
+    res =  [(p,1) for p in primes[:y]] + [(p,0) for p in primes[y:z+1]]
+    return res
+
+def greedy(H,primes_list):
+    '''
+    Performs a greedy siftin of H. For each prime in primelist it finds the one
+    that saves as much of H as possible if it is sifted by previous primes.
+    '''
+    for p in primes_list:
+        counts = count(H,p)
+        s = np.argmin(counts)
+        print 'greedy sifting with:',p,s,counts[s]
+        H = sift(H,[ (p,s) ])
+    return H
+
+def count(H,p): # expensive!
+    counts = []
+    for r in range(p):
+        cond = H % p == r
+        l = len(H[cond])
+        counts.append(l)
+        if l == 0: # there is a modulus that is not contained in H -> profit
+            break
+    return counts
+
+def pick_best(H,k):
+    '''
+    If our set H is larger than we need we just pick the subset with minimal diameter
+    '''
+    diameters = []
+    for i in range(len(H)-k+1):
+        diameters.append(H[i+k-1] - H[i])
+    i = np.argmin(diameters)
+    return H[i:i+k]
+
+def greedy_greedy(H,B):
+    primes = get_primes(max(abs(H)))
+    piB = pi(primes,sqrt(B))
+    sieve = [(2,1)] + [(p,0) for p in primes[1:piB]]
+    H = greedy(H,primes[piB:k])
+    H = sift(H,sieve)
+    return H,primes
+
+
 H,primes = process(H)
-results(H,primes)
+#H = np.arange(-185662,202456)
+#H,primes = greedy_greedy(H,4739224)
+results(H,primes) # this takes longest because is_admissible is expensive
+
+if len(H) > k:
+    H = pick_best(H,k)
+    print 'best diameter of length', k, 'is:',H[-1]-H[0]
--- prime_utils.py
+++ prime_utils.py
@@ -2,25 +2,22 @@ import math
 from bisect import bisect_left
 import numpy as np
 
-def get_primes():
-    return [2,3,5,7]
+def get_primes(max):
+    return primesfrom2to(max)
 
-def add_prime(primes):
-    prime = False
-    n = primes[-1]
-    while not prime:
-        n = n+2;
-        divisible = False
-        p = primes[0]
-        i = 0
-        while not divisible and p <= math.sqrt(n):
-            if n % p == 0:
-                divisible = True
-            i = i+1
-            p = primes[i]
-        if not divisible:
-            prime = True
-    primes.append(n)
+def primesfrom2to(n):
+    '''
+    Original author Robert Hanks
+    http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
+     Input n>=6, Returns a array of primes, 2 <= p < n
+    '''
+    sieve = np.ones(n/3 + (n%6==2), dtype=np.bool)
+    for i in xrange(1,int(n**0.5)/3+1):
+        if sieve[i]:
+            k=3*i+1|1
+            sieve[       k*k/3     ::2*k] = False
+            sieve[k*(k-2*(i&1)+4)/3::2*k] = False
+    return np.r_[2,3,((3*np.nonzero(sieve)[0][1:]+1)|1)]
 
 def is_prime(p,primes):
     'Locate the leftmost value exactly equal to x'
@@ -29,7 +26,7 @@ def is_prime(p,primes):
         return True
     else:
         return  False
-    
+
 def add_primes(N,primes):
     '''
     adds N primes
@@ -39,9 +36,15 @@ def add_primes(N,primes):
     return primes
 
 def is_p_admissible(H,p):
-#    return p != len(set(map(lambda x: x%p, H))) # std python
-    return p != len(np.unique(H % p)) # numpy - much better performance
-    
+    '''
+    This is the biggest bottleneck.
+    The following code is copied from numpy.unique with a change from quicksort
+    to mergesort since I found the latter to perform slightly better.
+    '''
+    r = H % p
+    r.sort(kind='mergesort')
+    return p != len(r[np.concatenate( ([True], r[1:] != r[:-1]) )])
+#    return p != len(np.unique(H % p))
 
 def is_admissible(H,primes):
     admissible = True
